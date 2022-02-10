3a3ff07f06c2ee900e8b52ceb054ee98d8d2ded0
Kristina Peru <krissy.germanov@gmail.com>
2022-02-09 20:29:01 -0700

    Pictures adeed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-console.log('%c HI', 'color: firebrick')
+// document.addEventListener('DOMContentLoaded', function () {
+    // fetchBreed();
+//   });
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+  const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+  
+  function fetchImg() {
+    fetch(imgUrl)
+    .then(res => res.json())
+    .then(results => {
+        results.message.forEach(image => renderImgs(image))
+    });
+}
+function renderImgs(pic){
+    let imgContainer = document.querySelector("#dog-image-container");
+    let picEl = document.createElement('img');
+    picEl.src = pic;
+    imgContainer.appendChild(picEl)
+}
+function fetchBreed() {
+    fetch(breedUrl)
+    .then(res => res.json())
+    .then(results => {
+        results.message.forEach(breed => renderBreeds(breed))
+    });
+}
+
+
+    fetchImg();
+   
