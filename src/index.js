5a0fc139bf3030ba14ace05ae9a15cff9af26636
Kristina Peru <krissy.germanov@gmail.com>
2022-02-10 02:42:38 -0700

    everything else

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
-// document.addEventListener('DOMContentLoaded', function () {
-    // fetchBreed();
-//   });
-  const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
-  const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
   
-  function fetchImg() {
+function fetchImg() {
     fetch(imgUrl)
     .then(res => res.json())
     .then(results => {
@@ -21,10 +18,50 @@ function fetchBreed() {
     fetch(breedUrl)
     .then(res => res.json())
     .then(results => {
-        results.message.forEach(breed => renderBreeds(breed))
+        breeds = Object.keys(results.message);
+        updateBreedList(breeds);
+        addBreedSelectListener();
     });
+}
+function updateBreedList(breeds) {
+    let ul = document.querySelector('#dog-breeds');
+    removeChildren(ul);
+    breeds.forEach(breed => addBreed(breed));
+}
+  
+function removeChildren(element) {
+    let child = element.lastElementChild;
+    while (child) {
+      element.removeChild(child);
+      child = element.lastElementChild;
+    }
+}
+  
+function selectBreedsStartingWith(letter) {
+    updateBreedList(breeds.filter(breed => breed.startsWith(letter)));
+}
+  
+function addBreedSelectListener() {
+    let breedDropdown = document.querySelector('#breed-dropdown');
+    breedDropdown.addEventListener('change', function (event) {
+      selectBreedsStartingWith(event.target.value);
+    });
+}
+  
+
+function addBreed(breed) {
+    let ul = document.querySelector('#dog-breeds')
+    let li = document.createElement('li');
+    li.innerText = breed;
+    console.log(breed)
+    li.style.cursor = 'pointer';
+    ul.appendChild(li);
+    li.addEventListener('click', updateColor)
+}
+function updateColor(e) {
+    e.target.style.color = '#b56fa1'
 }
 
 
     fetchImg();
-   
+    fetchBreed()
